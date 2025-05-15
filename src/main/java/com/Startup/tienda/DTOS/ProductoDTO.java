2c18f8609cdb50377cc9f7336a4b07db84720bba
Rafael-cmd-html <armandocastillejos086@gmail.com>
2025-05-14 19:24:12 -0700

    Arreglo de repositorio

--- src/main/java/com/Startup/tienda/DTOS/ProductoDTO.java
+++ src/main/java/com/Startup/tienda/DTOS/ProductoDTO.java
@@ -1,6 +1,7 @@
 package com.Startup.tienda.DTOS;
 
 public class ProductoDTO {
+    
     private int id;
     private String nombre;
     private String descripcion;
@@ -9,4 +10,63 @@ public class ProductoDTO {
     private byte[] imagen_producto;
 
     private int categoria_fk;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public Long getStock() {
+        return stock;
+    }
+
+    public void setStock(Long stock) {
+        this.stock = stock;
+    }
+
+    public byte[] getImagen_producto() {
+        return imagen_producto;
+    }
+
+    public void setImagen_producto(byte[] imagen_producto) {
+        this.imagen_producto = imagen_producto;
+    }
+
+    public int getCategoria_fk() {
+        return categoria_fk;
+    }
+
+    public void setCategoria_fk(int categoria_fk) {
+        this.categoria_fk = categoria_fk;
+    }
+
+    
+
 }
